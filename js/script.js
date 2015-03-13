1af7b8f31b2762c8e1e6e07913d18c919ca13710
danish21 <daninoorani21@gmail.com>
2015-03-13 04:45:33 -0500

    done

--- js/script.js
+++ js/script.js
@@ -69,33 +69,6 @@ $(document).ready(function(){
 	    event.preventDefault();
 	});
 
-	// var container = document.getElementById('visualization');
-
-  // Create a DataSet (allows two way data-binding)
-  // var items = new vis.DataSet([
-  //   {id: 1, content: 'item 1', start: '2014-04-20'},
-  //   {id: 2, content: 'item 2', start: '2014-04-14'},
-  //   {id: 3, content: 'item 3', start: '2014-04-18'},
-  //   {id: 4, content: 'item 4', start: '2014-04-16'},
-  //   {id: 5, content: 'item 5', start: '2014-04-25'},
-  //   {id: 6, content: 'item 6', start: '2014-04-27'}
-  //   {id: 7, content: 'item 6', start: '2014-04-27'}
-  // ]);
-
-  // // Configuration for the Timeline
-  // var options = {};
-
-  // // Create a Timeline
-  // var timeline = new vis.Timeline(
-  //   container,
-  //   items,
-  //   options
-  //   );
-	// $(window).resize(function() {
-	// 	headerHeight = $(window).height();
-	// 	$("#dynamicText").css('top', ""+ (headerHeight/2)+"px" );
-	// 	$("#searchBox").css('top', ""+ ((headerHeight/2)+50) +"px" );
-		
-	// });
+	
 	
 });
